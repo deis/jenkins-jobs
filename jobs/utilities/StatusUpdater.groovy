17fd1f73508beeca47900feb66e903a9447ddee2
Jonathan Chauncey <jchauncey@deis.com>
2016-04-25 12:04:15 -0600

    fix(status): Remove indents from status updater This should help in the config UI for jenkins

--- jobs/utilities/StatusUpdater.groovy
+++ jobs/utilities/StatusUpdater.groovy
@@ -2,19 +2,19 @@ package utilities
 
 class StatusUpdater {
   static updateStatus(Map args) {
-    """
-      #!/usr/bin/env bash
+"""
+#!/usr/bin/env bash
 
-      set -eo pipefail
+set -eo pipefail
 
-      curl\
-        --user deis-admin:"\${GITHUB_ACCESS_TOKEN}"\
-        --data '{\
-          "state":"${args.commitStatus}",\
-          "target_url":"'"\${BUILD_URL}"'",\
-          "description":"${args.description}",\
-          "context":"ci/jenkins/pr"}'\
-        "https://api.github.com/repos/deis/${args.repoName}/statuses/${args.commitSHA}"
-    """.stripIndent().trim()
+curl\
+--user deis-admin:"\${GITHUB_ACCESS_TOKEN}"\
+--data '{\
+"state":"${args.commitStatus}",\
+"target_url":"'"\${BUILD_URL}"'",\
+"description":"${args.description}",\
+"context":"ci/jenkins/pr"}'\
+"https://api.github.com/repos/deis/${args.repoName}/statuses/${args.commitSHA}"
+""".stripIndent().trim()
   }
 }
