69647cb7541dc99668e9b3cae00782fe35b449d6
Jason Hansen <jhansen@deis.com>
2016-06-10 11:24:44 -0700

    tests(deis.com): add pr test for deis.com

--- jobs/deis_com.groovy
+++ jobs/deis_com.groovy
@@ -1,12 +1,14 @@
 evaluate(new File("${WORKSPACE}/common.groovy"))
 
-name = 'deis.com'
+import utilities.StatusUpdater
+
+repo_name = 'deis.com'
 downstreamJobName = 'deploy_website'
 
-job(name) {
+job("deis_com_master") {
   description """
     <ol>
-      <li>Watches the <a href="https://github.com/deis/${name}">${name}</a> repo for a commit to master</li>
+      <li>Watches the <a href="https://github.com/deis/${repo_name}">${repo_name}</a> repo for a commit to master</li>
       <li>Kicks off downstream ${downstreamJobName} job to deploy docs</li>
     </ol>
   """.stripIndent().trim()
@@ -48,6 +50,93 @@ job(name) {
       set -eo pipefail
 
       make test
+    '''.stripIndent().trim()
+  }
+}
+
+job("deis_com_pr") {
+  description """
+    <ol>
+      <li>Watches the ${repo_name} repo_name for pull requests</li>
+    </ol>
+  """.stripIndent().trim()
+
+  scm {
+    git {
+      remote {
+        github("deis/${repo_name}")
+        credentials('597819a0-b0b9-4974-a79b-3a5c2322606d')
+        refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+      }
+      branch('${sha1}')
+    }
+  }
+
+  concurrentBuild()
+  throttleConcurrentBuilds {
+    maxPerNode(defaults.maxBuildsPerNode)
+    maxTotal(defaults.maxTotalConcurrentBuilds)
+  }
+
+  logRotator {
+    daysToKeep defaults.daysToKeep
+  }
+
+  triggers {
+    pullRequest {
+      admin('deis-admin')
+      cron('H/5 * * * *')
+      useGitHubHooks()
+      triggerPhrase('OK to test')
+      orgWhitelist(['deis'])
+      allowMembersOfWhitelistedOrgsAsAdmin()
+      // this plugin will update PR status no matter what,
+      // so until we fix this, here are our default messages:
+      extensions {
+        commitStatus {
+          context('ci/jenkins/pr')
+          triggeredStatus("Triggering ${repo_name} tests...")
+          startedStatus("Starting ${repo_name} tests...")
+          completedStatus("SUCCESS", "Merge with caution! Test job(s) may still be in progress...")
+          completedStatus("FAILURE", "Tests returned failure(s).")
+          completedStatus("ERROR", "Something went wrong.")
+        }
+      }
+    }
+  }
+
+  parameters {
+    stringParam('sha1', 'master', 'Specific Git SHA to test')
+  }
+
+  triggers {
+    githubPush()
+  }
+
+  wrappers {
+    timestamps()
+    colorizeOutput 'xterm'
+    credentialsBinding {
+    }
+  }
+
+  publishers {
+    slackNotifications {
+      projectChannel('#marketing')
+      notifyAborted()
+      notifyFailure()
+      notifySuccess()
+    }
+  }
+
+  steps {
+    shell '''
+      #!/usr/bin/env bash
+
+      set -eo pipefail
+
+      make test
+
     '''.stripIndent().trim()
   }
 }
