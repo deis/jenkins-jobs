d22972692d201ab30ce74932d180a90abf8c206b
Vaughn Dice <vdice@deis.com>
2016-07-15 16:03:06 -0400

    fix(common.groovy): check if WORKFLOW_CLI_SHA empty
    before overriding `CLI_VERSION`
    
    Ref https://github.com/deis/workflow-e2e/pull/260

--- common.groovy
+++ common.groovy
@@ -28,7 +28,10 @@ set -eo pipefail
 
 export WORKFLOW_CHART="workflow-${RELEASE}"
 export WORKFLOW_E2E_CHART="workflow-${RELEASE}-e2e"
-export CLI_VERSION="${WORKFLOW_CLI_SHA:0:7}"
+
+if [ -n "${WORKFLOW_CLI_SHA}" ]; then
+  export CLI_VERSION="${WORKFLOW_CLI_SHA:0:7}"
+fi
 
 mkdir -p ${E2E_DIR_LOGS}
 env > ${E2E_DIR}/env.file
