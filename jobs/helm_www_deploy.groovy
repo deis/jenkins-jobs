0889ae81a444370810c19cb138c6bbd2be3b6c61
Seth Goings <sgoings@engineyard.com>
2017-04-11 16:55:12 -0600

    fix(helm-www): use sgoings' helm-www repo

--- jobs/helm_www_deploy.groovy
+++ jobs/helm_www_deploy.groovy
@@ -15,7 +15,7 @@ job(name) {
   scm {
     git {
       remote {
-        github('helm/helm-www')
+        github('sgoings/helm-www')
         credentials('597819a0-b0b9-4974-a79b-3a5c2322606d')
       }
       branch('master')
