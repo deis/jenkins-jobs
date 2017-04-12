7e17a49df744aed8ff1784a5599430ef30663633
Seth Goings <sgoings@engineyard.com>
2017-04-12 16:09:04 -0600

    feat(deis-io): use dockerized + deis pull driven deployment

--- jobs/deis_io_deploy.groovy
+++ jobs/deis_io_deploy.groovy
@@ -52,14 +52,17 @@ job(name) {
     timestamps()
     colorizeOutput 'xterm'
     credentialsBinding {
-      file('DEIS_IO_STAGING_ENV', '2cfbe7b8-0e93-4e00-8c5b-1731d794d339')
-      file('DEIS_IO_PROD_ENV', 'a93c6610-b155-4718-9cb5-4ed7e6ba39e6')
       string("SLACK_INCOMING_WEBHOOK_URL", defaults.slack.webhookURL)
+      string("QUAY_PASSWORD", "8317a529-10f7-40b5-abd4-a42f242f22f0")
+      string("DEIS_URL", "368837f6-a5d1-4754-af84-0645473824a6")
+      string("DEIS_USERNAME", "342bc0d5-3c82-4e5c-aa79-8b4df0cc7298")
+      string("DEIS_PASSWORD", "201494de-a097-4a60-aaa3-6d16a930dabd")
     }
     parameters {
+      stringParam('QUAY_USERNAME', 'deis+jenkins', 'Quay account name')
+      stringParam('QUAY_EMAIL', 'deis+jenkins@deis.com', 'Quay email address')
       stringParam('DEIS_IO_ORG', 'deis', 'GitHub organization to use.')
       stringParam('DEIS_IO_BRANCH', 'gh-pages', 'deis.io branch to deploy')
-      stringParam('CONTAINER_ENV', '${DEIS_IO_PROD_ENV}', 'Environment file with AWS API Keys, S3 Buckets and CloudFront values')
     }
   }
 
@@ -69,8 +72,13 @@ job(name) {
 
       set -eo pipefail
 
-      cd "${WORKSPACE}"
-      make prep build deploy
+      docker login -e="\$QUAY_EMAIL" -u="\$QUAY_USERNAME" -p="\$QUAY_PASSWORD" quay.io
+      make prep build build-image push
+
+      curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
+      export PATH="\$(pwd):\$PATH"
+
+      make deploy
     '''.stripIndent().trim()
   }
 }
